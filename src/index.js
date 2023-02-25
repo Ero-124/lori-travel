213a1bbb2e7f300580d750e1f5cae7791752e1bf
Ero-124 <khachatryanero.2002@mail.ru>
2023-02-25 14:29:02 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const REPO_NAME = '/lori-travel/'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${REPO_NAME}/`}>
+    <BrowserRouter basename={`${REPO_NAME}`}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
