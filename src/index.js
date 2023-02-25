8a1712630ee160f55923ce9ab1cb5b23dc9db21e
Ero-124 <khachatryanero.2002@mail.ru>
2023-02-25 14:26:59 +0400

    LoriTravel :: Update basename #4

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import '../src/styles/index.css'
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 
+const REPO_NAME = '/lori-travel/'
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/`}>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
